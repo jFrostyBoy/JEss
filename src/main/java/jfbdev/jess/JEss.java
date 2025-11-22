1bf633ec51fc3da9040bae6b81a8c590e52665ba
jFrostyBoy <jfrostyboy01@gmail.com>
2025-11-22 23:26:34 +0200

    Register feed and heal commands in onEnable
    Added commands for feeding and healing players.

--- src/main/java/jfbdev/jess/JEss.java
+++ src/main/java/jfbdev/jess/JEss.java
@@ -1,8 +1,11 @@
 package jfbdev.jess;
 
+import jfbdev.jess.commands.JEssFeedCommand;
+import jfbdev.jess.commands.JEssHealCommand;
 import jfbdev.jess.commands.JEssReloadCommand;
 import jfbdev.jess.listeners.JoinQuitDeathListener;
 import jfbdev.jess.listeners.ServerMotdListener;
+import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import java.util.Objects;
@@ -13,10 +16,24 @@ public final class JEss extends JavaPlugin {
     public void onEnable() {
 
         saveDefaultConfig();
+
         getServer().getPluginManager().registerEvents(new JoinQuitDeathListener(this), this);
         getServer().getPluginManager().registerEvents(new ServerMotdListener(this), this);
 
         Objects.requireNonNull(getCommand("jessreload")).setExecutor(new JEssReloadCommand(this));
+        Objects.requireNonNull(getCommand("heal")).setExecutor(new JEssHealCommand(this));
+        Objects.requireNonNull(getCommand("feed")).setExecutor(new JEssFeedCommand(this));
 
+        Bukkit.getScheduler().runTaskLater(this, () -> {
+            Bukkit.getConsoleSender().sendMessage("");
+            Bukkit.getConsoleSender().sendMessage("§b    ░░░░░██╗███████╗░██████╗░██████╗");
+            Bukkit.getConsoleSender().sendMessage("§b    ░░░░░██║██╔════╝██╔════╝██╔════╝§6    Manager features at your server");
+            Bukkit.getConsoleSender().sendMessage("§b    ░░░░░██║█████╗░░╚█████╗░╚█████╗░§f    Plugin version: §bJESS-v1.1");
+            Bukkit.getConsoleSender().sendMessage("§b    ██╗░░██║██╔══╝░░░╚═══██╗░╚═══██╗§f    Author: §bjFrostyBoy");
+            Bukkit.getConsoleSender().sendMessage("§b    ╚█████╔╝███████╗██████╔╝██████╔╝§f    Website: §bhttps://jfrostyboy.42web.io");
+            Bukkit.getConsoleSender().sendMessage("§b    ░╚════╝░╚══════╝╚═════╝░╚═════╝░");
+            Bukkit.getConsoleSender().sendMessage("");
+            Bukkit.getConsoleSender().sendMessage("§8[§bJESS§8] §fThe plugin is §aenabled §fand ready to §awork!");
+        }, 20L);
     }
 }
